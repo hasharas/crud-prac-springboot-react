4d5b70ff695bfd85cd6bd1554d35268b27dbdd08
hasharas <hasharamsankalpam@gmail.com>
2025-09-05 16:01:05 +0530

    Create:entity file create

--- back/src/main/java/com/crud/mytest/entity/Employee.java
+++ back/src/main/java/com/crud/mytest/entity/Employee.java
@@ -1,20 +1,31 @@
-// package com.crud.mytest.entity;
+package com.crud.mytest.entity;
 
-// import jakarta.persistence.Entity;
-// import jakarta.persistence.GeneratedValue;
-// import jakarta.persistence.Id;
-// import jakarta.persistence.Table;
-// import lombok.AllArgsConstructor;
-// import lombok.Data;
-// import lombok.NoArgsConstructor;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-// @Entity
-// @Table(name="employee")
-// @Data
-// @NoArgsConstructor
-// @AllArgsConstructor
-// public class Employee {
+@Entity
+@Table(name = "employee")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee {
 
-// @Id
-// @GeneratedValue(strategy = GenerationType.IDENTITY)
-// }
+      @Id
+      @GeneratedValue(strategy = GenerationType.IDENTITY)
+      private Long id;
+
+      @Column(nullable = false)
+      private String name;
+
+      @Column(nullable = false, unique = true)
+      private String email;
+
+      private String department;
+}
