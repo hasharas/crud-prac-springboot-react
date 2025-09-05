3aaef9855824fcbc98c714ecd1503a4fd0080ab6
hasharas <hasharamsankalpam@gmail.com>
2025-09-05 15:51:38 +0530

    Connect:database connect and test

--- back/src/main/java/com/crud/mytest/entity/Employee.java
+++ back/src/main/java/com/crud/mytest/entity/Employee.java
@@ -1,5 +1,20 @@
-package com.crud.mytest.entity;
+// package com.crud.mytest.entity;
 
-public class Employee {
+// import jakarta.persistence.Entity;
+// import jakarta.persistence.GeneratedValue;
+// import jakarta.persistence.Id;
+// import jakarta.persistence.Table;
+// import lombok.AllArgsConstructor;
+// import lombok.Data;
+// import lombok.NoArgsConstructor;
 
-}
+// @Entity
+// @Table(name="employee")
+// @Data
+// @NoArgsConstructor
+// @AllArgsConstructor
+// public class Employee {
+
+// @Id
+// @GeneratedValue(strategy = GenerationType.IDENTITY)
+// }
